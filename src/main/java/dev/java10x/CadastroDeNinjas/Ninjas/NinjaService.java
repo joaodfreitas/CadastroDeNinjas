1a1ec36b6086c221af2147c1e8f092bc44b02dd1
João Freitas <defreitasjoaovitor7@gmail.com>
2026-04-29 19:28:39 -0300

    feat: Inclui a rota de cadastro de ninjas usando metódo POST, também inicializa o Postman

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -20,14 +20,14 @@ public class NinjaService {
     }
 
     // Listar todos os meus ninjas por ID
-    public NinjaModel listarNinjasPorId(Long id){
+    public NinjaModel listarNinjasPorId(Long id) {
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
         return ninjaPorId.orElse(null);
-
-
     }
 
-
-
+    // Criar um novo ninja
+    public NinjaModel criarNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
 
 }
